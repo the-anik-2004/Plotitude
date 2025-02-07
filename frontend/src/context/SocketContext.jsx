e5324ca4a22e7486ede0dac1241830d762e08938
Anik Pal <130134332+the-anik-2004@users.noreply.github.com>
2025-02-07 10:36:18 +0530

    Socket connection setup

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -9,7 +9,7 @@ export const SocketContextProvider=({children})=>{
     const { currentUser } = useContext(AuthContext);
 
     useEffect(()=>{
-       setSocket(io("http://localhost:4000"))
+       setSocket(io("https://plotitude-chat-server.onrender.com"))
     }
     ,[]);
 
